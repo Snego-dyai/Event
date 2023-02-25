5dd7f411c70ff0e1d014cb82c83e7bca4c5b5ef3
Snego-dyai <126317937+Snego-dyai@users.noreply.github.com>
2023-02-25 12:22:56 +0400

    Initial commit

---
+++ app/src/main/assets/www/app.js
@@ -0,0 +1,9 @@
+function printSomething() {
+   alert("Вы ввели: "+document.getElementById("name").value+".");
+
+
+}
+
+function document.getElementById("button").onclick = function() {
+  window.location.href = 'redirect-url';
+};
